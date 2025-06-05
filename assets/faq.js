8128faaa6a5bc252ae7e9255f55e65f68c5623ba
v.kuidin <v.kuidin@valantic.nl>
2025-06-05 16:04:05 +0300

    refac: use custom element approach for FAQ

--- assets/faq.js
+++ assets/faq.js
@@ -1,18 +1,22 @@
-function toggleFAQ(event) {
-    const titleClicked = event.target.closest('.faq-item__title');
-    if (!titleClicked) return;
+class FAQAccordion extends HTMLElement {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
 
-    const faqItem = titleClicked.closest('.faq-item');
-    if (faqItem) {
-        faqItem.classList.toggle('faq-item--open');
+    connectedCallback() {
+        this.addEventListener('click', this.handleClick);
+    }
+
+    handleClick(event) {
+        const titleClicked = event.target.closest('.faq-item__title');
+        if (!titleClicked) return;
+
+        const faqItem = titleClicked.closest('.faq-item');
+        if (faqItem) {
+            faqItem.classList.toggle('faq-item--open');
+        }
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('FAQ script loaded');
-    
-    const faqSection = document.querySelector('.faq-content');
-    if (faqSection) {
-        faqSection.addEventListener('click', toggleFAQ);
-    }
-})
+customElements.define('faq-accordion', FAQAccordion);
